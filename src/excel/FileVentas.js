6d367b1846321e079eebfd2916d55ed1773d9e43
Luis Lara <xanderls25@icloud.com>
2024-03-20 22:28:19 -0500

    Subiendo cambios a producción

--- src/excel/FileVentas.js
+++ src/excel/FileVentas.js
@@ -82,49 +82,49 @@ async function generateExcel(req, empresaInfo, data) {
         ws.column(10).setWidth(18);
         ws.column(11).setWidth(18);
 
-        ws.cell(1, 1, 1, 11, true).string(`${empresaInfo.nombreEmpresa}`).style(styleTitle);
-        ws.cell(2, 1, 2, 11, true).string(`RUC: ${empresaInfo.ruc}`).style(styleTitle);
-        ws.cell(3, 1, 3, 11, true).string(`${empresaInfo.direccion}`).style(styleTitle);
-        ws.cell(4, 1, 4, 11, true).string(`Celular: ${empresaInfo.celular} / Telefono: ${empresaInfo.telefono}`).style(styleTitle);
+        // ws.cell(1, 1, 1, 11, true).string(`${empresaInfo.nombreEmpresa}`).style(styleTitle);
+        // ws.cell(2, 1, 2, 11, true).string(`RUC: ${empresaInfo.ruc}`).style(styleTitle);
+        // ws.cell(3, 1, 3, 11, true).string(`${empresaInfo.direccion}`).style(styleTitle);
+        // ws.cell(4, 1, 4, 11, true).string(`Celular: ${empresaInfo.celular} / Telefono: ${empresaInfo.telefono}`).style(styleTitle);
 
-        ws.cell(6, 1, 6, 11, true).string(`REPORTE GENERAL DE VENTAS`).style(styleTitle);
-        ws.cell(7, 1, 7, 11, true).string(`PERIODO: ${dateFormat(req.query.fechaIni)} al ${dateFormat(req.query.fechaFin)}`).style(styleTitle);
+        // ws.cell(6, 1, 6, 11, true).string(`REPORTE GENERAL DE VENTAS`).style(styleTitle);
+        // ws.cell(7, 1, 7, 11, true).string(`PERIODO: ${dateFormat(req.query.fechaIni)} al ${dateFormat(req.query.fechaFin)}`).style(styleTitle);
 
-        ws.cell(9, 1).string(`Comprobante(s):`).style(styleHeader);
-        ws.cell(9, 2).string(`${req.query.idComprobante === '' ? "TODOS" : req.query.comprobante}`).style(styleHeader);
+        // ws.cell(9, 1).string(`Comprobante(s):`).style(styleHeader);
+        // ws.cell(9, 2).string(`${req.query.idComprobante === '' ? "TODOS" : req.query.comprobante}`).style(styleHeader);
 
-        ws.cell(10, 1).string(`Cliente(s):`).style(styleHeader);
-        ws.cell(10, 2).string(`${req.query.idCliente === '' ? "TODOS" : req.query.cliente}`).style(styleHeader);
+        // ws.cell(10, 1).string(`Cliente(s):`).style(styleHeader);
+        // ws.cell(10, 2).string(`${req.query.idCliente === '' ? "TODOS" : req.query.cliente}`).style(styleHeader);
 
-        ws.cell(11, 1).string(`Vendedor(s):`).style(styleHeader);
-        ws.cell(11, 2).string(`${req.query.idUsuario === '' ? "TODOS" : req.query.usuario}`).style(styleHeader);
+        // ws.cell(11, 1).string(`Vendedor(s):`).style(styleHeader);
+        // ws.cell(11, 2).string(`${req.query.idUsuario === '' ? "TODOS" : req.query.usuario}`).style(styleHeader);
 
-        ws.cell(9, 5).string(`Tipo(s):`).style(styleHeader);
-        ws.cell(9, 6).string(`${req.query.tipoVenta === 0 ? "TODOS" : req.query.tipo}`).style(styleHeader);
+        // ws.cell(9, 5).string(`Tipo(s):`).style(styleHeader);
+        // ws.cell(9, 6).string(`${req.query.tipoVenta === 0 ? "TODOS" : req.query.tipo}`).style(styleHeader);
 
-        const header = ["N°","Fecha", "N° Documento", "Información", "Comprobante", "Serie y Numeración", "Propiedad", "Tipo", "Estado", "Importe", "Anulado"];
+        // const header = ["N°","Fecha", "N° Documento", "Información", "Comprobante", "Serie y Numeración", "Propiedad", "Tipo", "Estado", "Importe", "Anulado"];
 
-        header.map((item, index) => ws.cell(13, 1 + index).string(item).style(styleTableHeader));
+        // header.map((item, index) => ws.cell(13, 1 + index).string(item).style(styleTableHeader));
 
-        data.map((item, index) => {
+        // data.map((item, index) => {
 
-            ws.cell(14 + index, 1).number(parseInt(index+1)).style(styleBodyInteger)
-            ws.cell(14 + index, 2).string(item.fecha).style(styleBody)
-            ws.cell(14 + index, 3).number(parseInt(item.documento)).style(styleBodyInteger)
-            ws.cell(14 + index, 4).string(item.informacion).style(styleBody)
-            ws.cell(14 + index, 5).string(item.comprobante).style(styleBody)
-            ws.cell(14 + index, 6).string(item.serie + "-" + item.numeracion).style(styleBody)
-            ws.cell(14 + index, 7).string(item.producto + " - " + item.categoria).style(styleBody)
-            ws.cell(14 + index, 8).string(item.tipo).style(styleBody)
-            ws.cell(14 + index, 9).string(item.estado).style(styleBody)
-            if (item.estado === "ANULADO") {
-                ws.cell(14 + index, 10).number(0).style(styleBodyFloat)
-                ws.cell(14 + index, 11).number(parseFloat(formatMoney(item.total))).style(styleBodyFloat)
-            } else {
-                ws.cell(14 + index, 10).number(parseFloat(formatMoney(item.total))).style(styleBodyFloat)
-                ws.cell(14 + index, 11).number(0).style(styleBodyFloat)
-            }
-        });
+        //     ws.cell(14 + index, 1).number(parseInt(index+1)).style(styleBodyInteger)
+        //     ws.cell(14 + index, 2).string(item.fecha).style(styleBody)
+        //     ws.cell(14 + index, 3).number(parseInt(item.documento)).style(styleBodyInteger)
+        //     ws.cell(14 + index, 4).string(item.informacion).style(styleBody)
+        //     ws.cell(14 + index, 5).string(item.comprobante).style(styleBody)
+        //     ws.cell(14 + index, 6).string(item.serie + "-" + item.numeracion).style(styleBody)
+        //     ws.cell(14 + index, 7).string(item.producto + " - " + item.categoria).style(styleBody)
+        //     ws.cell(14 + index, 8).string(item.tipo).style(styleBody)
+        //     ws.cell(14 + index, 9).string(item.estado).style(styleBody)
+        //     if (item.estado === "ANULADO") {
+        //         ws.cell(14 + index, 10).number(0).style(styleBodyFloat)
+        //         ws.cell(14 + index, 11).number(parseFloat(formatMoney(item.total))).style(styleBodyFloat)
+        //     } else {
+        //         ws.cell(14 + index, 10).number(parseFloat(formatMoney(item.total))).style(styleBodyFloat)
+        //         ws.cell(14 + index, 11).number(0).style(styleBodyFloat)
+        //     }
+        // });
 
         return wb.writeToBuffer();
     } catch (error) {
