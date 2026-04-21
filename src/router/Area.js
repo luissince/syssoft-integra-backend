bbdb97b557112f14a1ef16dbc4d2799903f61ffc
Luis Lara <xanderls25@icloud.com>
2026-04-21 06:49:43 -0500

    Unir cambios

--- src/router/Area.js
+++ src/router/Area.js
@@ -8,9 +8,13 @@ router.post('/', async (req, res) => await area.add(req, res));
 
 router.put('/', async (req, res) => await area.update(req, res));
 
+<<<<<<< HEAD
 router.get('/combo', async (req, res) => await area.combo(req, res));
 
 router.get('/:idArea/id', async (req, res) => await area.id(req, res));
+=======
+router.get('/:idArea', async (req, res) => await area.id(req, res));
+>>>>>>> 480f14b (adding cargos, area)
 
 router.delete('/:idArea', async (req, res) => await area.delete(req, res));
 
