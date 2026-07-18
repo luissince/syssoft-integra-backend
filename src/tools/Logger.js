9766fba9da9dcbc605bf69a9a54a273d57867756
Luis Lara <xanderls25@icloud.com>
2026-07-18 14:44:27 -0500

    Subir cambios

--- src/tools/Logger.js
+++ src/tools/Logger.js
@@ -1,32 +1,53 @@
-const winston = require('winston');
-const DailyRotateFile = require('winston-daily-rotate-file');
+const winston = require("winston");
+const DailyRotateFile = require("winston-daily-rotate-file");
+
+
+const onlyLevel = (level) => {
+    return winston.format((info) => {
+        return info.level === level ? info : false;
+    })();
+};
+
 
 const logger = winston.createLogger({
+
     format: winston.format.combine(
         winston.format.timestamp(),
-        winston.format.printf(({ level, message, timestamp }) => {
-            return `${new Date(timestamp).toLocaleString()} ${level}: ${message}`;
+        winston.format.errors({ stack: true }),
+        winston.format.printf(({ level, message, timestamp, stack }) => {
+            return `${new Date(timestamp).toLocaleString()} ${level}: ${stack || message}`;
         })
     ),
+
     transports: [
-        new winston.transports.Console(),
 
         new DailyRotateFile({
-            filename: "logs/app-%DATE%.log",
+            filename: "logs/info-%DATE%.log",
             datePattern: "YYYY-MM-DD",
-            level: "info",
+            format: onlyLevel("info"),
             maxSize: "20m",
-            maxFiles: "2d",
+            maxFiles: "7d",
+        }),
+
+        new DailyRotateFile({
+            filename: "logs/warn-%DATE%.log",
+            datePattern: "YYYY-MM-DD",
+            format: onlyLevel("warn"),
+            maxSize: "20m",
+            maxFiles: "15d",
         }),
 
         new DailyRotateFile({
             filename: "logs/error-%DATE%.log",
             datePattern: "YYYY-MM-DD",
-            level: "error",
+            format: onlyLevel("error"),
             maxSize: "20m",
-            maxFiles: "30d",
+            maxFiles: "60d",
         }),
-    ],
+
+        new winston.transports.Console()
+    ]
 });
+
 
 module.exports = logger;
